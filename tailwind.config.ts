37e8a7b31263137499f81abfadca7c8ca7dec3d1
Matt <MHatmaker72@gmail.com>
2025-11-21 04:38:37 +0000

    yes the index.html "<!DOCTYPE html> <html lang="en">   <head>     <meta

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Merriweather', 'serif'],
-        headline: ['MedievalSharp', 'cursive'],
+        body: ['Inter', 'sans-serif'],
+        headline: ['Space Grotesk', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
