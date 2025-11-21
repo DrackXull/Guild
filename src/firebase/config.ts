f8f8537d259375209caf76f1a2c379741c8dbee0
Matt <MHatmaker72@gmail.com>
2025-11-21 20:04:27 +0000

    why is the ProjectID you generated not the one i sent you         and ap

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-3453259482-a7135",
-  "appId": "1:337124473282:web:e62f1b725425a9fc03fe5b",
-  "apiKey": "AIzaSyChcNCKgHBx8RJcqGLUD-b1afqM3Z_0aWM",
-  "authDomain": "studio-3453259482-a7135.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "337124473282"
+  apiKey: "AIzaSyBvToh8BEwqsKvSrOMDFtb3zsbzGEtcABc",
+  authDomain: "guild-nexus.firebaseapp.com",
+  projectId: "guild-nexus",
+  storageBucket: "guild-nexus.appspot.com",
+  messagingSenderId: "574251812274",
+  appId: "1:574251812274:web:e3cc5d57227fdcd71ad0ab",
+  measurementId: "G-YW0RR8DH3T"
 };
