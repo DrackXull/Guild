026478a5e86171d04a9beddde76b41110a378dc1
Matt <MHatmaker72@gmail.com>
2025-11-21 07:27:12 +0000

    Try fixing this error: `Runtime Error: ReferenceError: CardFooter is not

--- src/app/(app)/market/page.tsx
+++ src/app/(app)/market/page.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Store, Gem } from "lucide-react";
 import { mockPlayer } from "@/lib/data";
 import { Button } from "@/components/ui/button";
